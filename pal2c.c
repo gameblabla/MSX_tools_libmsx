adc4cf8850febac52a2ff108141eb55df6847621
gameblabla <gameblabla@protonmail.com>
2023-03-11 12:07:30 +0100

    Fix pal2c tool

--- pal2c.c
+++ pal2c.c
@@ -1,43 +1,47 @@
 #include <stdio.h>
-#include <stdint.h>
+#include <stdlib.h>
 
-#define ARRAY_SIZE 16
+#define BUFFER_SIZE 32
 
 int main(int argc, char *argv[]) {
-	int c = 0;
-    if (argc < 3) {
-        fprintf(stderr, "Usage: %s filename output\n", argv[0]);
+    FILE *input_file, *output_file;
+    char buffer[BUFFER_SIZE];
+    int read_count, i;
+
+    // Open input file
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <input_file>\n", argv[0]);
+        return 1;
+    }
+    input_file = fopen(argv[1], "rb");
+    if (input_file == NULL) {
+        fprintf(stderr, "Error: Could not open file %s\n", argv[1]);
         return 1;
     }
 
-	FILE *filetowrite = fopen(argv[2], "w");
-	
-    FILE *file = fopen(argv[1], "rb");
-    if (file == NULL) {
-        fprintf(stderr, "Error: unable to open file %s\n", argv[1]);
+    // Move file pointer to end of file and read last BUFFER_SIZE bytes
+    fseek(input_file, -BUFFER_SIZE, SEEK_END);
+    read_count = fread(buffer, 1, BUFFER_SIZE, input_file);
+    if (read_count != BUFFER_SIZE) {
+        fprintf(stderr, "Error: Could not read last %d bytes of file %s\n", BUFFER_SIZE, argv[1]);
         return 1;
     }
 
-    uint16_t data[ARRAY_SIZE];
-    size_t bytes_read = fread(data, sizeof(uint16_t), ARRAY_SIZE, file);
-    fclose(file);
-
-    fprintf(filetowrite, "const unsigned short msx_palette[%d] = {\n", ARRAY_SIZE);
-    for (int i = 0; i < bytes_read; i++) {
-        uint16_t value = data[i];
-        
-        fprintf(filetowrite, "    0x%04X", value);
-        if (i < 15) fprintf(filetowrite, ",");
-		
-		c++;
-        if (c > 3) {
-            fprintf(filetowrite, "\n");
-            c = 0;
-        }
+    // Output C array to file
+    output_file = fopen(argv[2], "w");
+    if (output_file == NULL) {
+        fprintf(stderr, "Error: Could not open file output.c for writing\n");
+        return 1;
     }
-    fprintf(filetowrite, "};\n");
+    fprintf(output_file, "const char my_array[%d] = {\n", BUFFER_SIZE);
+    for (i = 0; i < BUFFER_SIZE; i++) {
+        fprintf(output_file, "0x%02x, ", (unsigned char) buffer[i]);
+    }
+    fprintf(output_file, "};\n");
 
-	fclose(filetowrite);
+    // Close input and output files
+    fclose(input_file);
+    fclose(output_file);
 
     return 0;
 }
